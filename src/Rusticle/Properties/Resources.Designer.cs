8ae7419d6c042995142c8fb6e0a85a706baa75de
Zachary Yates <zachary.yates@gmail.com>
2014-03-11 14:15:19 -0700

    added simpler packaging

--- src/Rusticle/Properties/Resources.Designer.cs
+++ src/Rusticle/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace Rusticle.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap chevron_reticle_green {
-            get {
-                object obj = ResourceManager.GetObject("chevron_reticle_green", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap chevron_reticle_red {
             get {
                 object obj = ResourceManager.GetObject("chevron_reticle_red", resourceCulture);
